71ad029e2df92f650d5fc0c2e7a345fe905bd327
Teslov-Mikhail <wisinski@mail.ru>
2017-08-14 19:29:28 +0200

    First commit
    It should work.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # IkariamNotifier
 Simple and straightforward notifier for Ikariam
+---
+Prosty i szybki notifier do gry Ikariam
+
+## Jak używać
+Potrzebujesz pluginu do używanej przeglądarki który ułatwia obsługę UserScripts.
+Polecam Tampermonkey
+Chrome: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
+Firefox: https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/
+Opera: https://addons.opera.com/en-gb/extensions/details/tampermonkey-beta/
+
+Po instalacji dodatku, kliknięcie na poniższy link automatycznie doda skrypt do przeglądarki.
+https://raw.githubusercontent.com/Teslov-Mikhail/IkariamNotifier/master/script.js
+
+Gotowe, wszystko działa.
+Prawdopodobnie musisz odświeżyć stronę z Ikariamem.
+
+## Dotacje
+Chcesz podziękować za skrypt?
+Podrzuć parę groszy przez PayPala
+https://www.paypal.me/teslov
---
+++ script.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Ikariam Notifications
+// @namespace    https://twitter.com/Mikhail_Teslov
+// @version      0.1
+// @description  Pretty simple and straightforward.
+// @author       Mikhail Teslov
+// @match        *ikariam.gameforge.com/*
+// @grant        none
+// @downloadURL https://raw.githubusercontent.com/Teslov-Mikhail/IkariamNotifier/master/script.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var alertSound = new Audio("https://raw.githubusercontent.com/Teslov-Mikhail/IkariamNotifier/master/waralert.mp3");
+
+    var notificationOptions = {
+      body: "Twoje miasto wymaga natychmiastowej interwencji.",
+      icon: "https://raw.githubusercontent.com/Teslov-Mikhail/IkariamNotifier/master/general.png"
+    }
+
+    function checkForAlerts() {
+      if ( $("#js_GlobalMenu_military").hasClass("normalalert") ) {
+
+        alertSound.play();
+
+        if (!("Notification" in window)) {
+          alert("Twoja przeglądarka nie opsługuje powiadomień.");
+        }
+
+        else if (Notification.permission === "granted") {
+          var notification = new Notification("Twój Generał jest zaalarmowany!", notificationOptions);
+        }
+
+        else if (Notification.permission !== "denied") {
+          Notification.requestPermission(function (permission) {
+            if (permission === "granted") {
+              notification = new Notification("Twój Generał jest zaalarmowany!", notificationOptions);
+            }
+          });
+        }
+      }
+
+      setTimeout(function () {
+        checkForAlerts();
+      }, 60000);
+    }
+    checkForAlerts();
+})();
